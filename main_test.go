30b4b94c252fab41d62888ff07fc3d07cf3a92cc
Gareth Evans <g.evans@sap.com>
2022-03-24 07:00:19 +0000

    Recursively search from starting point

--- main_test.go
+++ main_test.go
@@ -15,15 +15,76 @@ import (
 )
 
 type Pom struct {
-	Name       string `xml:"name"`
+	Name          string       `xml:"name"`
+	ArtifactId    string       `xml:"artifactId"`
+	GroupId       string       `xml:"groupId"`
+	ParentGroupId string       `xml:"parent>groupId"`
+	Version       string       `xml:"version"`
+	ParentVersion string       `xml:"parent>version"`
+	Dependencies  []Dependency `xml:"dependencies>dependency"`
+	Properties    Properties   `xml:"properties"`
+	License       string       `xml:"licenses>license>name"`
+	LicenseUrl    string       `xml:"licenses>license>url"`
+}
+
+func (p *Pom) GetGroupId() string {
+	if p.GroupId != "" {
+		return p.GroupId
+	}
+	return p.ParentGroupId
+}
+
+func (p *Pom) GetVersion() string {
+	if p.Version != "" {
+		return p.Version
+	}
+	return p.ParentVersion
+}
+
+type Dependency struct {
 	ArtifactId string `xml:"artifactId"`
 	GroupId    string `xml:"groupId"`
 	Version    string `xml:"version"`
+	Scope      string `xml:"scope"`
+}
+
+type Properties struct {
+	Property []Property `xml:",any"`
+}
+
+func (p *Properties) Find(name string) string {
+	for _, prop := range p.Property {
+		if "${"+prop.Name+"}" == name {
+			return prop.Value
+		}
+	}
+	return ""
+}
+
+type Property struct {
+	Name  string
+	Value string
+}
+
+func (v *Property) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	v.Name = start.Name.Local
+	return d.DecodeElement(&v.Value, &start)
+}
+
+func (d *Dependency) JarPath() string {
+	return fmt.Sprintf("/%s/%s/%s/%s-%s.jar", strings.ReplaceAll(d.GroupId, ".", "/"), d.ArtifactId, d.Version, d.ArtifactId, d.Version)
+}
+
+func (d *Dependency) PomPath() string {
+	return fmt.Sprintf("/%s/%s/%s/%s-%s.pom", strings.ReplaceAll(d.GroupId, ".", "/"), d.ArtifactId, d.Version, d.ArtifactId, d.Version)
 }
 
 func TestSomething(t *testing.T) {
-	//url := "https://repo1.maven.org/maven2/com/sap/cloud/sjb/xs-env/1.24.0/xs-env-1.24.0.jar"
-	url := "https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.13.2/jackson-core-2.13.2.jar"
+	d := Dependency{
+		ArtifactId: "xs-jdbc-routing-datasource",
+		GroupId:    "com.sap.cloud.sjb",
+		Version:    "1.27.0",
+	}
 
 	dir, err := ioutil.TempDir("", "dependency-manager")
 	assert.NoError(t, err)
@@ -32,28 +93,76 @@ func TestSomething(t *testing.T) {
 		os.RemoveAll(dir)
 	})
 
-	err = DownloadFile(filepath.Join(dir, filepath.Base(url)), url)
+	err = ResolveFrom(d, dir)
 	assert.NoError(t, err)
+}
+
+func ResolveFrom(dependency Dependency, dir string) error {
+	fmt.Printf("ResolveFrom(%+v)\n", dependency)
+
+	url := "https://repo1.maven.org/maven2" + dependency.JarPath()
+	err := DownloadFile(filepath.Join(dir, filepath.Base(url)), url)
+	if err != nil {
+		return err
+	}
 
 	sha, err := Sha256(filepath.Join(dir, filepath.Base(url)))
-	assert.NoError(t, err)
+	if err != nil {
+		return err
+	}
 
-	pom, err := GetPom(strings.TrimSuffix(url, ".jar") + ".pom")
-	assert.NoError(t, err)
+	//fmt.Println("getting pom...")
+	pomUrl := "https://repo1.maven.org/maven2" + dependency.PomPath()
+	pom, err := GetPom(pomUrl)
+	if err != nil {
+		return err
+	}
 
 	fmt.Println("  [[metadata.dependencies]]")
-	fmt.Printf("    cpes = [\"cpe:2.3:a:%s:%s:%s:*:*:*:*:*:*:*\"]\n", pom.GroupId, pom.ArtifactId, pom.Version)
+	fmt.Printf("    cpes = [\"cpe:2.3:a:%s:%s:%s:*:*:*:*:*:*:*\"]\n", pom.GetGroupId(), pom.ArtifactId, pom.GetVersion())
 	fmt.Printf("    id = \"%s\"\n", pom.ArtifactId)
 	fmt.Printf("    name = \"%s\"\n", pom.Name)
-	fmt.Printf("    purl = \"pkg:generic/%s@%s\"\n", pom.ArtifactId, pom.Version)
+	fmt.Printf("    purl = \"pkg:generic/%s@%s\"\n", pom.ArtifactId, pom.GetVersion())
 	fmt.Printf("    sha256 = \"%s\"\n", sha)
-	fmt.Println("    stacks = [\"io.buildpacks.stacks.bionic\", \"io.buildpacks.stacks.tiny\"]")
+	fmt.Println("    stacks = [\"io.buildpacks.stacks.bionic\", \"io.buildpacks.stacks.tiny\", \"*\"]")
 	fmt.Printf("    uri = \"%s\"\n", url)
-	fmt.Printf("    version = \"%s\"\n", pom.Version)
+	fmt.Printf("    version = \"%s\"\n", pom.GetVersion())
 	fmt.Println("")
-	fmt.Println("    [[metadata.dependencies.licenses]]")
-	fmt.Println("    type = \"BSD-2-Clause\"")
-	//fmt.Println("    uri = \"https://jdbc.postgresql.org/about/license.html\"")
+	if pom.License != "" {
+		fmt.Println("    [[metadata.dependencies.licenses]]")
+		fmt.Printf("      type = \"%s\"\n", pom.License)
+		if pom.LicenseUrl != "" {
+			fmt.Printf("      uri = \"%s\"\n", pom.LicenseUrl)
+		}
+	}
+	fmt.Println("")
+
+	for _, d := range pom.Dependencies {
+		if d.Scope != "test" {
+			var dependency Dependency
+			if strings.HasPrefix(d.Version, "${") {
+				dependency = Dependency{
+					ArtifactId: d.ArtifactId,
+					GroupId:    d.GroupId,
+					Version:    pom.Properties.Find(d.Version),
+				}
+			} else {
+				dependency = Dependency{
+					ArtifactId: d.ArtifactId,
+					GroupId:    d.GroupId,
+					Version:    d.Version,
+				}
+			}
+
+			if dependency.Version != "" {
+				err = ResolveFrom(dependency, dir)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func Sha256(filepath string) (string, error) {
@@ -72,7 +181,6 @@ func DownloadFile(filepath string, url string) error {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("writing", filepath)
 	// Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
@@ -93,19 +201,21 @@ func GetPom(url string) (Pom, error) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return Pom{}, fmt.Errorf("Status error: %v", resp.StatusCode)
+		return Pom{}, fmt.Errorf("status error: %v", resp.StatusCode)
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return Pom{}, fmt.Errorf("Read body: %v", err)
+		return Pom{}, fmt.Errorf("read body: %v", err)
 	}
 
 	var result Pom
 	err = xml.Unmarshal(data, &result)
 	if err != nil {
-		return Pom{}, fmt.Errorf("Read body: %v", err)
+		return Pom{}, fmt.Errorf("parse: %v", err)
 	}
+
+	fmt.Println("pom=", result)
 
 	return result, nil
 }
